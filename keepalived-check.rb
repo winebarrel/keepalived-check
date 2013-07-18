9e98be91e90079281cd4ca6afe09349fb0fea422
Genki Sugawara <sugawara@cookpad.com>
2013-07-18 16:49:39 +0900

    fix comment

--- keepalived-check.rb
+++ keepalived-check.rb
@@ -290,7 +290,7 @@ Accept = Proc.new do
 		try_accept Aipmask, :dev, Anetif
 		try_accept Aipmask, :dev, Anetif, :scope, Regexp.union(*%w[site link host nowhere global_defs])
 		try_accept Aipmask, :dev, Anetif, :scope, Regexp.union(*%w[site link host nowhere global_defs]), :table, Aany
-		# XXX: I did not know keepalived or accept what the table id...
+		# XXX: I do not know what table-id is accepted...
 	end
 end
 
